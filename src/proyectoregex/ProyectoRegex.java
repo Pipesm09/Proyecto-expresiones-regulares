dac967c7561ebe88da3e6be2acd624b579a0162f
Pipesm09 <juansafe2@gmail.com>
2026-09-24 13:56:05 -0500

    Que ricochet

--- src/proyectoregex/ProyectoRegex.java
+++ src/proyectoregex/ProyectoRegex.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyectoregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,6 +13,37 @@ public class ProyectoRegex {
     
     public static void main(String[] args) {
         
+        String regex,cadena;
+        int cont=0;
+        Pattern p;
+        Matcher m;
+        regex = "Ola";
+        cadena="ola hola carola que rica que esta la ola\nola, "
+                + "ola invita a Magola para que traiga las polas\nola y "
+                + "salgamos al sol a escuchar la radiola";
+        
+        p = Pattern.compile(regex,Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+        m = p.matcher(cadena);
+        StringBuilder sb = new StringBuilder();
+        while(m.find()){
+            cont++;
+            sb.append("desde ").append(m.start()).append(" hasta ").append(m.end()).append(" en ").append(m.group()).append("\n");
+        }
+        
+        if(cont==0){
+            JOptionPane.showMessageDialog(null, "no es valido");
+        }
+        else{
+            String mensaje = String.format("El se encontró %d veces en la cadena\nEstá en las siguientes posiciones:\n%s",cont ,sb.toString());
+            JOptionPane.showConfirmDialog(null, mensaje);
+        }
+        /*if(m.matches()){
+            JOptionPane.showMessageDialog(null, "Es válido");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "No es válido");
+        }
+        */
     }
     
 }
